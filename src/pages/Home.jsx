8899b3c23108a191fb0e9ac72b50d23a06e33cdb
genadi-kolev <genadi.kolev@csitea.net>
2023-12-27 00:49:10 +0200

    change: move about page contents to landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
 import Fade from 'react-reveal';
 import Typewriter from 'typewriter-effect';
 import FallbackSpinner from '../components/FallbackSpinner';
@@ -13,16 +15,36 @@ const styles = {
     display: 'inline-block',
   },
   mainContainer: {
-    height: '100%',
+    height: '100vh',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
   },
+  introTextContainer: {
+    margin: 10,
+    flexDirection: 'column',
+    whiteSpace: 'pre-wrap',
+    textAlign: 'left',
+    fontSize: '1.2em',
+    fontWeight: 500,
+  },
+  introImageContainer: {
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+  },
 };
 
 function Home() {
   const { data } = useEndpoint('home');
+
+  const parseIntro = (text) => (
+    <ReactMarkdown
+      children={text}
+    />
+  );
 
   return data ? (
     <Fade>
@@ -40,7 +62,21 @@ function Home() {
         </div>
         <Social />
       </div>
+
+      <div className="section-content-container">
+        <Container>
+          <Row>
+            <Col style={styles.introTextContainer}>
+              {parseIntro(data.about)}
+            </Col>
+            <Col style={styles.introImageContainer}>
+              <img src={data?.imageSource} alt="profile" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </Fade>
+
   ) : <FallbackSpinner />;
 }
 
