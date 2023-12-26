530ee3a25814a2ef67eaedb7f0fdc053ba9915c1
genadi-kolev <genadi.kolev@csitea.net>
2023-12-27 00:03:34 +0200

    feat: extract common json fetching logic into custom hook

---
+++ src/components/useEndpoint.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import endpoints from '../constants/endpoints';
+
+const useEndpoint = (endpoint) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(endpoints[endpoint], {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => err);
+  }, []);
+
+  return { data };
+};
+
+export default useEndpoint;
