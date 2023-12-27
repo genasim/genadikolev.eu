ba9097e2c17a2eed90327f8408d42336d346bd17
genadi-kolev <genadi.kolev@csitea.net>
2023-12-27 23:57:34 +0200

    feat: make pages arr for different routes

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+const pages = [
+  { title: 'Home', path: '/' },
+  { title: 'Skills', path: '/skills' },
+  { title: 'Resume', path: '/resume' },
+  { title: 'Projects', path: '/projects' },
+];
+
+export default pages;
