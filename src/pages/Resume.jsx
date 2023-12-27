415e6c95e19cfc4fb5306b8c5136b13b5422c50d
genadi-kolev <genadi.kolev@csitea.net>
2023-12-27 17:05:17 +0200

    feat: add functional timeline to resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,93 +1,83 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React, { useContext } from 'react';
-import { Container } from 'react-bootstrap';
-import ReactMarkdown from 'react-markdown';
-import Fade from 'react-reveal';
+import { MdBusinessCenter } from 'react-icons/md';
+import { PiStudentFill } from 'react-icons/pi';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import { ThemeContext } from 'styled-components';
-import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 import FallbackSpinner from '../components/FallbackSpinner';
 import useEndpoint from '../components/useEndpoint';
 import '../css/experience.css';
 
-const styles = {
-  ulStyle: {
-    listStylePosition: 'outside',
-    paddingLeft: 20,
-  },
-  subtitleContainerStyle: {
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  subtitleStyle: {
-    display: 'inline-block',
-  },
-  inlineChild: {
-    display: 'inline-block',
-  },
-  itemStyle: {
-    marginBottom: 10,
-  },
+const resolveCardType = (type, theme) => {
+  let Icon = null;
+  let cardBg = null;
+
+  switch (type) {
+    case 'work':
+      Icon = MdBusinessCenter;
+      cardBg = theme.accentColor;
+      break;
+    case 'study':
+      Icon = PiStudentFill;
+      cardBg = 'pink';
+      break;
+    default:
+      Icon = MdBusinessCenter;
+      cardBg = theme.accentColor;
+      break;
+  }
+  return { Icon, cardBg };
 };
 
 function Resume() {
   const theme = useContext(ThemeContext);
-
   const { data } = useEndpoint('resume');
 
+  if (!data) return <FallbackSpinner />;
+
   return (
-    <>
-      {data
-        ? (
-          <div className="section-content-container">
-            <Container>
-              <Timeline
-                lineColor={theme.timelineLineColor}
-              >
-                {data.map((item) => (
-                  <Fade key={item.title + item.dateText}>
-                    <TimelineItem
-                      dateText={item.dateText}
-                      dateInnerStyle={{ background: theme.accentColor }}
-                      style={styles.itemStyle}
-                      bodyContainerStyle={{ color: theme.color }}
-                    >
-                      <h2 className="item-title">
-                        {item.title}
-                      </h2>
-                      <div style={styles.subtitleContainerStyle}>
-                        <h4 style={{ ...styles.subtitleStyle, color: theme.accentColor }}>
-                          {item.subtitle}
-                        </h4>
-                        {item.workType && (
-                        <h5 style={styles.inlineChild}>
-                    &nbsp;·
-                          {' '}
-                          {item.workType}
-                        </h5>
-                        )}
-                      </div>
-                      <ul style={styles.ulStyle}>
-                        {item.workDescription.map((point) => (
-                          <div key={point}>
-                            <li>
-                              <ReactMarkdown
-                                children={point}
-                                components={{
-                                  p: 'span',
-                                }}
-                              />
-                            </li>
-                            <br />
-                          </div>
-                        ))}
-                      </ul>
-                    </TimelineItem>
-                  </Fade>
-                ))}
-              </Timeline>
-            </Container>
-          </div>
-        ) : <FallbackSpinner /> }
-    </>
+    <VerticalTimeline>
+      {data.map((item) => {
+        const { Icon, cardBg } = resolveCardType(item.type, theme);
+        return (
+          <VerticalTimelineElement
+            key={item.name}
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: cardBg, color: theme.color }}
+            contentArrowStyle={{
+              borderRight: `7px solid  ${cardBg}`,
+            }}
+            date={item.date}
+            iconStyle={{ background: cardBg, color: theme.color }}
+            icon={<Icon />}
+          >
+            <h3 className="vertical-timeline-element-title">{item.name}</h3>
+            <h5 className="vertical-timeline-element-subtitle">{item.place}</h5>
+            <br />
+            {item.roles.map((role) => (
+              <div key={`${item.name}-${role.title}`}>
+                <h3>{role.title}</h3>
+                {role.occupation && <h5>{role.occupation}</h5>}
+                <ul>
+                  {role.descriptions.map((point) => (
+                    <li key={point}>{point}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </VerticalTimelineElement>
+        );
+      })}
+
+      <VerticalTimelineElement
+        iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+        // icon={<StarIcon />}
+      />
+    </VerticalTimeline>
   );
 }
 
