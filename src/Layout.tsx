e13a3afc9f3ca62b19eba1a36ca814a8c6dfae47
genadi-kolev <genadi.kolev@csitea.net>
2024-02-14 00:15:31 +0200

    change: add responsive padding contraints for pages to fit them inbetween menus

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react'
+import styled from 'styled-components'
+import media from 'styled-media-query'
 import Navbar from './components/Navbar'
 import SocialsSidebar from './components/SocialsSidebar'
 
@@ -6,12 +8,26 @@ interface LayoutProps {
   children: ReactNode
 }
 
+const PageContainer = styled.div`
+  padding: 6rem 8rem;
+
+  ${media.lessThan('medium')`
+    padding: 6rem 2rem;
+  `}
+
+  ${media.lessThan('small')`
+    padding: 6rem 1rem;
+  `}
+`
+
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Navbar />
       <SocialsSidebar />
-      {children}
+      <PageContainer>
+        {children}
+      </PageContainer>
     </>
   )
 }
