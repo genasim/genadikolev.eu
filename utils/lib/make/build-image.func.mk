f50594c762638df3df6da09a63de703527cf3904
genadi-kolev <genadi.kolev@csitea.net>
2024-03-06 11:16:02 +0200

    feat: expose base build args as ENVs for subsequent images

--- utils/lib/make/build-image.func.mk
+++ utils/lib/make/build-image.func.mk
@@ -1,8 +1,9 @@
 define build-img
 	$(eval DOCKERFILE_PATH=${1})
-	$(eval NO_CACHE=${2})
+	$(eval TAG=${2})
+	$(eval NO_CACHE=${3})
 
-	@docker build . -t base-image $(NO_CACHE) \
+	@docker build . -t $(TAG) $(NO_CACHE) \
 		--build-arg UID=${UID} \
 		--build-arg GID=${GID} \
 		--build-arg APPUSR=${APPUSR} \
