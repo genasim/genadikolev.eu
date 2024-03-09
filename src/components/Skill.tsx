c1993bd0842e2a2c086b8f553ad0182e416cc85e
Genadi Kolev <genadimkolev@gmail.com>
2024-03-09 23:39:06 +0200

    faet: add animated fill in progress bar

---
+++ src/components/Skill.tsx
@@ -0,0 +1,35 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+
+interface SkillProps {
+  progress?: number
+  name?: string
+}
+
+const AnimatedProgressBar: React.FC<{progress?: number}> = ({ progress }) => (
+    <div
+        className="bg-white rounded-3"
+        style={{ height: '0.7rem' }}
+      >
+        <motion.div
+          initial={{ width: 0 }}
+          animate={{ width: progress + '%' }}
+          transition={{ duration: 1 }}
+          className={`bg-primary h-100 ${progress === 100 ? "rounded-3" : "rounded-start-3"}`}
+        />
+      </div>
+)
+
+const Skill: React.FC<SkillProps> = ({ progress, name }) => {
+  return (
+    <div>
+      <div className="d-flex justify-content-between">
+        <h4>{name}</h4>
+        <h4>{progress}%</h4>
+      </div>
+      <AnimatedProgressBar progress={progress} />
+    </div>
+  )
+}
+
+export default Skill
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,6 +1,15 @@
+import Skill from '../components/Skill'
+
 const SkillsPage = () => {
-    return <h1>Skills page</h1>
-  }
-  
-  export default SkillsPage
-  
+  return (
+    <div>
+      <h1>Skills page</h1>
+      <Skill progress={85} name='Javascript' />
+      <Skill progress={80} name='React.js' />
+      <Skill progress={60} name='Docker' />
+      <Skill progress={100} name='Docker' />
+    </div>
+  )
+}
+
+export default SkillsPage
